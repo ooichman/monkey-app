1aaffa791fb7656be8f2df4f38ef5732c1657151
Oren Oichman <ooichman@redhat.com>
2021-03-28 13:30:02 +0300

    Monkey App

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,15 @@
-FROM ubi8/go-toolset as build
+FROM golang:alpine as build
 
 WORKDIR /opt/app-root
 COPY src src
 WORKDIR /opt/app-root/src/monkey/
 RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o monkey
 
-FROM ubi8/ubi-minimal
+FROM scratch
 
 WORKDIR /opt/app-root
 COPY --from=build /opt/app-root/src/monkey/monkey /opt/app-root/monkey
+COPY index.html /opt/app-root/
 
 EXPOSE 8080
 ENTRYPOINT ["./monkey"]
--- src/monkey/main.go
+++ src/monkey/main.go
@@ -2,19 +2,65 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func loadPage(filename string) (*Page, error) {
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Page{Title: filename, Body: body}, nil
+}
 
 // HelloServer responds to requests with the given URL path.
 func HelloServer(w http.ResponseWriter, r *http.Request) {
+
 	fmt.Fprintf(w, "Hello, you requested: %s\n", r.URL.Path)
 	log.Printf("Received request for path: %s", r.URL.Path)
 }
-func main() {
-	var addr string = ":8080"
-	handler := http.HandlerFunc(HelloServer)
-	if err := http.ListenAndServe(addr, handler); err != nil {
-		log.Fatalf("Could not listen on port %s %v", addr, err)
+
+func ReadFile(w http.ResponseWriter, r *http.Request) {
+	h_workdir, found := os.LookupEnv("HELLO_WORKDIR")
+	if !found {
+		h_workdir = "/opt/app-root/"
 	}
+
+	file_name, f_found := os.LookupEnv("HELLO_FILENAME")
+	if !f_found {
+		file_name = "index.html"
+	}
+
+	h_workdir += "/" + file_name
+
+	p, err := loadPage(h_workdir)
+
+	if err != nil {
+		p = &Page{Title: file_name}
+	}
+
+	h_workdir += "index.html"
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func main() {
+	port, found := os.LookupEnv("GO_PORT")
+	if !found {
+		port = "8080"
+	}
+
+	http.HandleFunc("/api/", HelloServer)
+	http.HandleFunc("/readfile/", ReadFile)
+
+	log.Printf("Starting to listen on port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
